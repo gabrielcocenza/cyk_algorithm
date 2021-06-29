ee6daace53b8d081a3befe8ddb54f07954cf775d
Gabriel Cocenza <gabriel.cocenza@canonical.com>
2021-06-28 23:36:22 -0300

    output and final results matching

---
+++ README.md
@@ -0,0 +1,26 @@
+# ☀ ACH2043 - Introdução à Teoria da Computação Exercício de Programação: Implementação do algoritmo CYK para Gramáticas Livres do Contexto
+
+Este programa lê dois arquivos de entrada `inp-cadeia.txt` e `inp-glc.txt`, executa o algoritmo CYK sobre as cadeias de input. Quando aceita escreve 1 e quando recusada escreve 0 no arquivo `out-status.txt`
+
+## Dependências
+* Python >= 3.6
+
+## IDE utilizada
+* VSCode
+
+## Sistema Operacional de Desenvolvimento
+* Ubuntu 20.04 (Focal LTS)
+
+## Execução do programa
+
+Para executar o programa, uma vez no diretório do código fonte execute o seguinte comando no terminal:
+
+```bash
+python glc.py
+```
+
+Caso o python2 seja padrão na máquina, tente o seguinte comando:
+
+```bash
+python3 glc.py
+```
--- glc.py
+++ glc.py
@@ -1,9 +1,17 @@
+# Gabriel Angelo Sgarbi Cocenza nºUSP: 6448118
+
 import os
 
 def read_file(filename):
     filename = os.path.join(os.curdir, filename)
     with open(filename, 'r') as f:
         return f.read().splitlines()
+
+
+def write_output_file(results) -> None:
+    with open("out-status.txt", "w") as f:
+        for result in results:
+            f.write(" ".join(result))
 
 
 def get_rules(rows):
@@ -16,41 +24,35 @@ def get_rules(rows):
         n_var, n_symbol, n_rule =  map(int, rows[cyk_index].split(' '))
         vars = rows[cyk_index + 1].split(' ')
         symbols = rows[cyk_index + 2].split(' ')
+        s0 = vars[0]
         assert len(vars) == n_var
         assert len(symbols) == n_symbol
-        rules[current_cyk] = {}
-        # print(rules)
+        rules[current_cyk] = {'s0': s0}
 
         for i in range(cyk_index + 3, cyk_index + 3 + n_rule):
-            left, right = rows[i].split(' => ')
+            left, right = rows[i].replace(' ', '').split('=>')
             if len(right) > 1:
                 if not rules[current_cyk].get(right):
                     rules[current_cyk][right] = set()
                 rules[current_cyk][right].add(left)
             else:
+                if not rules[current_cyk].get('initial'):
+                    rules[current_cyk]['initial'] = []
+                
+                if left == s0:
+                    rules[current_cyk]['initial'].append(right) 
+                
                 if not rules[current_cyk].get('terminals'):
                     rules[current_cyk]['terminals'] = {}
+                
                 if not rules[current_cyk]['terminals'].get(right):
                     rules[current_cyk]['terminals'][right] = []
                 rules[current_cyk]['terminals'][right].append(left)
-            # print(rules[current_cyk])
 
-
-
-        # print()
-        # print()
         cyk_index = cyk_index + 3 + n_rule
         current_cyk += 1
     
     return rules, n_cyk
-
-
-def init_rules(vars):
-    rules = {}
-    for var in vars:
-        rules[var] = {'terminals': [], 'non_terminals': []}
-
-    return rules
 
 
 def get_inputs(rows, n_cyk):
@@ -69,13 +71,14 @@ def get_inputs(rows, n_cyk):
 
     return inputs
 
+
 def cyk_alg(rule, inputs):
     result = []
 
     for input in inputs:
         #cadeias de tamano 1
         if len(input) == 1:
-            if input in list(rule['terminals'].keys()):
+            if input in rule['initial']:
                 result.append('1')
             else:
                 result.append('0')
@@ -89,50 +92,38 @@ def cyk_alg(rule, inputs):
 
             #subcadeias tamanho >= 2
             for l in range(2, len(input) + 1):
-                # print(f'l = {l}')
                 for i in range(0, len(input) - l + 1):
-                    # print(f'i = {i}')
                     j = i + l -1
-                    # print(f'j = {j}')
                     for k in range(i, j):
-                        print(f'table[{i}][{k}]')
-                        print(f'table[{k+1}][{j}]')
                         w = list(table[i][k])
                         x = list(table[k+1][j])
                         if len(w) > 0:
                             for elem_w in w:
                                 for elem_x in x:
                                     a = f'{elem_w} {elem_x}'
-                                    print(a)
-                                    match = rule.get(f'{elem_w} {elem_x}')
-                                    print(match)
+                                    match = rule.get(f'{elem_w}{elem_x}')
                                     if match:
                                         table[i][j].update(match)
-                        print(f'l = {l}, i = {i}, j = {j}, k = {k}')
-                        pass
 
-
-            if table[0][len(input) - 1]:
+            #Se 𝑆0∈𝑀(1,𝑛), aceite; senão, rejeite
+            if rule['s0'] in table[0][len(input) - 1]:
                 result.append('1')
             else:
                 result.append('0')
-            print(input)
-            for row in table:
-                print(row)
-            print()
-            
+
     return result
 
 
 def main():
-    rows_grammar = read_file('teste-glc.txt')
-    rows_input = read_file('teste-cadeias.txt')
+    results = []
+    rows_grammar = read_file('inp-glc.txt')
+    rows_input = read_file('inp-cadeias.txt')
     rules, n_cyk = get_rules(rows_grammar)
     inputs = get_inputs(rows_input, n_cyk)
-    print(f'rules: {rules[1]}')
-    print(f'inputs {inputs[1]}')
-    result = cyk_alg(rules[1], inputs[1])
-    print(f'final result: {result}')
+    for key in rules.keys():
+        results.append(cyk_alg(rules[key], inputs[key]) + ["\n"])
+    
+    write_output_file(results)
 
 
 if __name__ == "__main__":
